a447ddb9da2c4cde73dd7649392931c55445370e
thehaseebshah <haseebshah@users.noreply.github.com>
2026-07-25 21:53:04 +0500

    feat: implement modern tactile slate design philosophy and Product Sans font

--- .eleventy.js
+++ .eleventy.js
@@ -716,6 +716,7 @@ module.exports = function(eleventyConfig) {
     return content;
   });
 
+  eleventyConfig.addPassthroughCopy("src/site/fonts");
   eleventyConfig.addPassthroughCopy("src/site/img");
   eleventyConfig.addPassthroughCopy("src/site/scripts");
   eleventyConfig.addPassthroughCopy("src/site/styles/_theme.*.css");
