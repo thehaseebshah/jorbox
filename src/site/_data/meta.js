a44d91e4ee98fcb252413485c79482da659fe91f
thehaseebshah <haseebshah@users.noreply.github.com>
2026-07-26 12:26:05 +0500

    feat: change site name to Shabab JorBox

--- src/site/_data/meta.js
+++ src/site/_data/meta.js
@@ -94,7 +94,7 @@ module.exports = async (data) => {
     noteIconsSettings,
     timestampSettings,
     baseTheme: process.env.BASE_THEME || "dark",
-    siteName: process.env.SITE_NAME_HEADER || "Digital Garden",
+    siteName: process.env.SITE_NAME_HEADER || "Shabab JorBox",
     siteLogoPath: logoPath,
     mainLanguage: process.env.SITE_MAIN_LANGUAGE || "en",
     siteBaseUrl: baseUrl,
